429a3e07b02061decff50fc239e2c41e4ec1216a
tdudkowski <tdudkowski@gmail.com>
2019-01-21 04:06:17 +0100

    first stage of working

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
 const boardFields = [...document.querySelectorAll("section.board > div")];
 const btn = document.querySelector('button');
 
+const count = () => {
+ let thisCounter = 0;
+ boardFields.forEach(boardField => {
+  let fieldValue = [...boardField.dataset.key];
+  let y = fieldValue[0];
+  let x = fieldValue[1];
+  let left = Math.abs(Number(x == 0 ? 0 : `${boardField.dataset.key -1}`));
+  left = ("0" + left).slice(-2);
+  let right = Math.abs(Number(`${y}${(x-(-1))}`));
+  right = ("0" + right).slice(-2);
+  let top = Math.abs(Number(`${y-1}${x}`));
+  top = ("0" + top).slice(-2);
+  let down = Math.abs(Number(`${y-(-1)}${(x)}`));
+  down = ("0" + down).slice(-2);
+  boardField.textContent = `l${left}, r${right}, t${top}, d${down}, id${boardField.dataset.key}`;
+
+
+  if (document.querySelector(`[data-key='${left}']`).classList.contains('black')) {
+   thisCounter++;
+  }
+  if (document.querySelector(`[data-key='${right}']`).classList.contains('black')) {
+   thisCounter++;
+  }
+  if (document.querySelector(`[data-key='${top}']`).classList.contains('black')) {
+   thisCounter++;
+  }
+  if (document.querySelector(`[data-key='${down}']`).classList.contains('black')) {
+   thisCounter++;
+  }
+  console.log(boardField.dataset.key, thisCounter);
+
+  if (thisCounter < 2) {
+   boardField.classList.toggle('black')
+  }
+
+  if (thisCounter == 2) {
+   boardField.classList.add('black')
+  }
+
+  if (thisCounter == 4) {
+   boardField.classList.toggle('black')
+  }
+
+  thisCounter = 0;
+ });
+}
+
 const render = () => {
  boardFields.forEach(boardField => boardField.addEventListener('click', () => {
   boardField.classList.toggle('black');
@@ -11,18 +58,4 @@ const render = () => {
 
 render();
 
-const move = () => {
- boardFields.forEach(boardField => {
-  let fieldValue = [...boardField.dataset.key];
-  let y = fieldValue[0];
-  let x = fieldValue[1];
-  let left = Number(x == 0 ? 0 : `${boardField.dataset.key -1}`);
-  let right = Number(`${y}${(x-(-1))}`);
-  let top = Number(`${y-1}${x}`);
-  let down = Number(`${y-(-1)}${(x)}`);
-  boardField.textContent = `l${left}, r${right}, t${top}, d${down}, id${boardField.dataset.key}, x${x}, y${y}`;
-  // boardField.textContent = boardField.dataset.key;
- });
-}
-
-btn.addEventListener('click', move)
+btn.addEventListener('click', count)
